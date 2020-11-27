0b818964a4934121aa019923d23b435bd1216453
Tsuwa16 <tk.suwa16@gmail.com>
2020-11-28 00:49:11 +0900

    new

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Home v-if="!isLogin"></Home>
+    <Editor v-if="isLogin"></Editor>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home"
+import Editor from "./components/Editor"
+  
+export default {
+  name:app,
+
+  data(){
+    return{
+      isLogin:false
+    }
+  },
+  components:{
+    Home:Home,
+    Editor:Editor
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="home">
+      <h1>{{msg}}</h1>
+      <button @click="googleLogin">Googleアカウントでログイン</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:"home",
+
+    data(){
+        return{
+            msg: "Welcome"
+        }
+    },
+    methods:{
+        googleLogin:function(){
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
